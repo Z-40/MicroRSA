99a7561c222160d2198310e9375758a35d829e69
Z-40 <58406y4@gmail.com>
2021-03-22 13:42:25 +0530

    Added file description and license information

--- blinding.py
+++ blinding.py
@@ -1,3 +1,19 @@
+#  Licensed under the General Public License, Version 3.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+"""Contains all functions related to using blinding for signing and
+decrypting using RSA, for more information on blinding see:
+https://en.wikipedia.org/wiki/Blinding_(cryptography)"""
+
 import math
 from MicroRSA.common import get_rand_num, modular_inv
 
