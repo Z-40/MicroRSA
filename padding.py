b4a7c6b48e652fa1e38cf66182a7a150e456e37c
Z-40 <58406y4@gmail.com>
2021-03-22 15:19:25 +0530

    Added file description and license information

--- padding.py
+++ padding.py
@@ -1,3 +1,22 @@
+#  Licensed under the General Public License, Version 3.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+"""Contains all functions related to padding messages before signing 
+and encryption, for more information see https://en.wikipedia.org/wiki/
+Padding_(cryptography)#:~:text=In%20cryptography%2C%20padding%20is%20
+any,a%20message%20prior%20to%20encryption."""
+
+
+
 import os
 from MicroRSA.exceptions import PaddingError
 
