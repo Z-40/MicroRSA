3059730f2e1c97dad548cc10eebfb597f0ac31dd
Z-40 <58406y4@gmail.com>
2021-03-23 23:13:44 +0530

    Change docstrings

--- blinding.py
+++ blinding.py
@@ -19,10 +19,8 @@ from MicroRSA.common import get_rand_num, modular_inv
 
 
 def get_blinding_factor(n):
-    """
-    Get blinding factors r and r_inverse where r is approxmately ``n``
-    bits in length and r and n are relatively prime
-    """
+    """Get blinding factors r and r_inverse where r is approxmately ``n``
+    bits in length and r and n are relatively prime"""
     blindr = None
     while True:
         r = get_rand_num(n - 1)
@@ -36,10 +34,8 @@ def get_blinding_factor(n):
 
 
 def blinded_operation(c, n, e, d):
-    """
-    Decrypt or sign a message using to blinding prevent side channel attacks
-    See: https://en.wikipedia.org/wiki/Side-channel_attack
-    """
+    """Decrypt or sign a message using to blinding prevent side channel attacks
+    See: https://en.wikipedia.org/wiki/Side-channel_attack"""
     b, binv = get_blinding_factor(n)
     blinded = (pow(b, e, n) * c) % n
 
