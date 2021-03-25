97a8dd6559ebca9d5b4b80b8f3c6796cbd822f85
Z-40 <58406y4@gmail.com>
2021-03-25 12:49:51 +0530

    Remove unused variables

--- blinding.py
+++ blinding.py
@@ -19,18 +19,16 @@ from MicroRSA.common import get_rand_num, modular_inv
 
 
 def get_blinding_factor(n):
-    """Get blinding factors r and r_inverse where r is approxmately ``n``
+    """Get blinding factors r and r_inverse where r is approximately ``n``
     bits in length and r and n are relatively prime"""
-    blindr = None
     while True:
         r = get_rand_num(n - 1)
         if math.gcd(r, n) == 1:
-            blindr = r
             break
 
-    blindr_inv = modular_inv(blindr, n)
+    r_inv = modular_inv(r, n)
 
-    return blindr, blindr_inv
+    return r, r_inv
 
 
 def blinded_operation(c, n, e, d):
