387e36a76ab0520b7cfc57f783f7c51918124617
Z-40 <58406y4@gmail.com>
2021-03-26 15:24:56 +0530

    Add copyright info

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+# This call to setup() does all the work
+setup(
+    name="micro_rsa",
+    version="1.0",
+    description="Use RSA encryption in your projects",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Z-40/MicroRSA",
+    author="Z-40",
+    author_email=None,
+    license="GPLv3",
+    classifiers=[
+        "License :: OSI Approved :: General Public License, Version 3.0",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
