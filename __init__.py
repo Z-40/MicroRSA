137c664715dfa6e589dd92aae5bfdbaae69428e1
Z-40 <58406y4@gmail.com>
2021-03-27 20:16:46 +0530

    Add extra functionality

--- __init__.py
+++ __init__.py
@@ -1,51 +1,23 @@
 # Copyright (C) 2021 Z-40
 
-# from rsa import get_key_strength
-# from rsa import private2public
-# from rsa import encrypt
-# from rsa import decrypt
-# from rsa import newkeys
-# from rsa import verify
-# from rsa import sign
+from micro_rsa.pkcs import get_key_strength
+from micro_rsa.pkcs import private2public
+from micro_rsa.pkcs import encrypt
+from micro_rsa.pkcs import decrypt
+from micro_rsa.pkcs import newkeys
+from micro_rsa.pkcs import verify
+from micro_rsa.pkcs import sign
 
-from micro_rsa.pkcs import *
+from micro_rsa.prime import get_primes
+from micro_rsa.prime import get_prime
 
-# from prime import get_primes
-# from prime import get_prime
-#
-# from common import get_rand_num
-# from common import modular_inv
-# from common import byte_size
-# from common import bytes2int
-# from common import int2bytes
-# from common import egcd
-#
-# from exceptions import
+from micro_rsa.common import get_rand_num
+from micro_rsa.common import modular_inv
+from micro_rsa.common import byte_size
+from micro_rsa.common import bytes2int
+from micro_rsa.common import int2bytes
+from micro_rsa.common import egcd
 
-__all__ = [
-    "get_key_strength",
-    "private2public",
-    "AbstractKey",
-    "PrivateKey",
-    "PublicKey",
-    "newkeys",
-    "encrypt",
-    "decrypt",
-    "verify",
-    "sign"
-]
+# from micro_rsa.exceptions import
 
 print(dir())
-=======
-=======
->>>>>>> parent of 0b70dc9 (Add copyright)
-from MicroRSA.rsa import get_key_strength
-from MicroRSA.rsa import private2public
-from MicroRSA.rsa import encrypt
-from MicroRSA.rsa import decrypt
-from MicroRSA.rsa import newkeys
-from MicroRSA.rsa import verify
-from MicroRSA.rsa import sign
-
-from MicroRSA.prime import get_primes
->>>>>>> parent of 0b70dc9 (Add copyright)
