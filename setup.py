5293d2fcd6128d801ba4e78a74ef7b5914d3d37f
Z-40 <58406y4@gmail.com>
2021-04-03 01:57:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ DESCRIPTION = "A pure python implementation of the RSA encryption algorithm"
 
 setup(
     name="u_micro_rsa",
-    version="2021.1",
-    license="GPL v3",
+    version="2021.2",
+    license="GNU General Public License Version 3.0",
     author="Z-40",
     description=DESCRIPTION,
     url="https://github.com/Z-40/MicroRSA",
     install_requires=[],
     keywords=["python", "RSA", "encryption", "public-key-cryptography"],
-    packages=["micro_rsa"]
+    packages=["micro_rsa"],
+    license_file="microrsa/license.txt"
 )
